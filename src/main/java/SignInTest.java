5df822c29b5a0685602d32295df19e081d59ac66
praveepop <53791933+praveepop@users.noreply.github.com>
2019-08-06 20:04:56 +0530

    Update SignInTest.java
    Instantiating driver objects correctly and added code fro frame handling

--- src/main/java/SignInTest.java
+++ src/main/java/SignInTest.java
@@ -7,23 +7,30 @@ import org.testng.annotations.Test;
 
 public class SignInTest {
 
-    WebDriver driver = new ChromeDriver();
+	WebDriver driver =null;
 
     @Test
     public void shouldThrowAnErrorIfSignInDetailsAreMissing() {
 
         setDriverPath();
-
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        //Navigating to URL
         driver.get("https://www.cleartrip.com/");
         waitFor(2000);
-
+        //Clicking on YourTrips options
         driver.findElement(By.linkText("Your trips")).click();
         driver.findElement(By.id("SignIn")).click();
-
+        
+        //Switching to Frame
+        driver.switchTo().frame("modal_window");
+        //Clicking on SigninButton
         driver.findElement(By.id("signInButton")).click();
-
         String errors1 = driver.findElement(By.id("errors1")).getText();
+        //Verifying error message 
         Assert.assertTrue(errors1.contains("There were errors in your submission"));
+        //Switching to default 
+        driver.switchTo().defaultContent();
         driver.quit();
     }
 
